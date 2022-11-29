74416864aedc5db8a296a927df01a22600a05f05
Alex Paden <padenfool@gmail.com>
2022-11-28 20:15:59 -0800

    Basic functionality rebuilt (#1)
    Reduced packages (remove util)
    
    Restructure functions with error handling
    
    Format result json, function parameters
    
    Rename core functions

--- balances/balances.go
+++ balances/balances.go
@@ -2,111 +2,168 @@ package balances
 
 import (
 	"context"
+	"errors"
 	"log"
 	"math/big"
+	"os"
+	"regexp"
 
-	"github.com/alexpaden/go-crypto-service/token"
-	util "github.com/alexpaden/go-crypto-service/utils"
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
+	"github.com/shopspring/decimal"
 )
 
 type Wallet struct {
-	ADDRESS  string
-	BALANCES []Balance
+	ADDRESS  string    `json:"address"`
+	BALANCES []Balance `json:"balances"`
 }
 
-// should be lowercase?
 type Balance struct {
-	CHAINID int
-	BALANCE *big.Float
+	CHAINID int             `json:"chain_id"`
+	BALANCE decimal.Decimal `json:"balance"`
 }
 
-// used for many balances as goroutine
-func GoGetSingleBal(address string, chainId int, ch chan Balance) {
+func RetrieveSingleBal(address string, chainId int) (*Wallet, error) {
+	if !isValidAddress(address) {
+		err := errors.New("invalid address")
+		return nil, err
+	}
+	if !isValidChainId(chainId) {
+		err := errors.New("invalid chainId")
+		return nil, err
+	}
 	account := common.HexToAddress(address)
 	wallet := Wallet{
 		ADDRESS: account.String(),
 	}
-
-	client, err := ethclient.Dial(util.InfuraStringMaker(chainId))
-	if err != nil {
-		log.Fatal(err)
-	}
-	wei, err := client.BalanceAt(context.Background(), account, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//eth := util.ToDecimal(wei, 18)
-	eth := new(big.Float)
-	eth.SetString(wei.String())
-	wallet.BALANCES = append(wallet.BALANCES, Balance{CHAINID: chainId, BALANCE: eth})
-
-	client.Close()
-	ch <- wallet.BALANCES[0]
-}
-
-func GetManyBalances(address string) Wallet {
-	account := common.HexToAddress(address)
-	wallet := Wallet{
-		ADDRESS: account.String(),
-	}
-
-	chainIds := []int{1, 5, 137}
-
+	// ---------
 	ch := make(chan Balance)
+	go xRetrieveBal(address, chainId, ch)
+	balance := <-ch
+	wallet.BALANCES = append(wallet.BALANCES, balance)
+	// -------
+	return &wallet, nil
+}
 
+func RetrieveManyBalances(address string) (*Wallet, error) {
+	if !isValidAddress(address) {
+		err := errors.New("invalid address")
+		return nil, err
+	}
+
+	account := common.HexToAddress(address)
+	wallet := Wallet{
+		ADDRESS: account.String(),
+	}
+
+	// ---------
+	ch := make(chan Balance)
+	chainIds := []int{1, 5, 137}
 	for _, chainId := range chainIds {
-		go GoGetSingleBal(address, chainId, ch)
+		go xRetrieveBal(address, chainId, ch)
 	}
 	balances := make([]Balance, len(chainIds))
-
 	for i := range balances {
 		balances[i] = <-ch
 	}
-
 	wallet.BALANCES = balances
+	// -------
 
-	return wallet
+	return &wallet, nil
 }
 
-func GetTokenBalance(walletAddress string, chainId int, contractAddress string) Wallet {
-
-	client, err := ethclient.Dial(util.InfuraStringMaker(chainId))
-	tokenAddress := common.HexToAddress(contractAddress)
-	address := common.HexToAddress(walletAddress)
-	wallet := Wallet{
-		ADDRESS: address.String(),
-	}
-
-	// create a new instance of the token contract bound to a specific deployed contract
-	instance, err := token.NewToken(tokenAddress, client)
+func xRetrieveBal(address string, chainId int, ch chan Balance) {
+	account := common.HexToAddress(address)
+	client, err := ethclient.Dial(infuraStringMaker(chainId))
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-
-	// call BalanceOf function that takes an address and returns a *big.Int
-	bal, err := instance.BalanceOf(&bind.CallOpts{}, address)
+	wei, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
+	}
+	client.Close()
+	bal := weiToDecimalRounded(wei, 18)
+	ch <- Balance{CHAINID: chainId, BALANCE: bal}
+}
+
+// -----------------------------
+/*
+ Insert Cool divider here
+ This divides things nicely
+ I like that
+ Divide Away, my friend.
+*/
+// -----------------------------
+
+func isValidChainId(chainId int) bool {
+	switch chainId {
+	case 1, 5, 42, 137, 80001:
+		return true
+	default:
+		return false
+	}
+}
+
+// retrieves env variables from ./.env file
+func goGetDotEnv(key string) string {
+	println()
+	err := godotenv.Load("./.env")
+	if err != nil {
+		log.Panicf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
+// creates an infura connection string by chainId
+func infuraStringMaker(chainId int) string {
+	url := "https://"
+	switch chainId {
+	case 1:
+		url = url + "mainnet"
+	case 5:
+		url = url + "goerli"
+	case 42:
+		url = url + "kovan"
+	case 137:
+		url = url + "polygon-mainnet"
+	case 80001:
+		url = url + "polygon-mumbai"
+	default:
+		log.Panicf("requested chainId %v is not supported on infura, try {1, 5, 137}", chainId)
 	}
 
-	// name of token contract
-	//name, err := instance.Name(&bind.CallOpts{})
+	return url + ".infura.io/v3/" + goGetDotEnv("INFURA_KEY")
+}
 
-	// convert wei to eth
-	//balance := util.ToDecimal(bal, 18)
-	//fbal := new(big.Float)
-	//fbal.SetString(bal.String())
-	//balance := new(big.Float).Quo(fbal, big.NewFloat(math.Pow10(int(decimals))))
+// IsValidAddress validate hex address
+func isValidAddress(iaddress interface{}) bool {
+	re := regexp.MustCompile("^0x[0-9a-fA-F]{40}$")
+	switch v := iaddress.(type) {
+	case string:
+		return re.MatchString(v)
+	case common.Address:
+		return re.MatchString(v.Hex())
+	default:
+		return false
+	}
+}
 
-	//fmt.Printf("balance: %f", balance) // "balance: 74605500.647409"
+// ToDecimal wei to decimals
+func weiToDecimalRounded(ivalue interface{}, decimals int) decimal.Decimal {
+	decimal.MarshalJSONWithoutQuotes = true
+	value := new(big.Int)
+	switch v := ivalue.(type) {
+	case string:
+		value.SetString(v, 10)
+	case *big.Int:
+		value = v
+	}
 
-	decimals := util.ToDecimal(bal, 18)
-
-	wallet.BALANCES = append(wallet.BALANCES, Balance{CHAINID: chainId, BALANCE: decimals})
-
-	return wallet
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	num, _ := decimal.NewFromString(value.String())
+	result := num.Div(mul)
+	result = result.RoundDown(3)
+	return result
 }
